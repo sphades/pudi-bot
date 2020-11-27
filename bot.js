f89d748c360b4f64a1d8b5a44d705ad535a436ba
sphades <40293652+sphades@users.noreply.github.com>
2020-11-27 23:38:24 +0800

    crash fix

--- bot.js
+++ bot.js
@@ -19,27 +19,24 @@ client.once("disconnect", () => {
 let users = new Map();
 let newUserChannel = ''
 let defaultSong = 'https://www.youtube.com/watch?v=KZ9l85fOq1Y'
-// let songMap = new Map();
 let booleanMap = new Map();
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-    if (newMember.id === '781557440902463508') return;
-    if (newMember === null) return;
+    //ideally just check that its someone joining the server\
+    if (!oldMember.channel.members.size-1){ //if only one in server, leave
+        oldMember.channel.leave();
+    }
+    if (newMember.id === '781557440902463508') return; //if the new addition is the bot, ignore
     if (booleanMap.get(newMember.guild.id) === undefined) {
         booleanMap.set(newMember.guild.id, true)
         console.log('Add Bool for this server')
     }
-    // if (songMap.get(newMember.guild.id) === undefined) {
-    //     songMap.set(newMember.guild.id, defaultSong)
-    //     console.log('missing song')
-    // }
     if (users.get(newMember.member.id) === undefined){
         users.set(newMember.member.id, defaultSong)
         console.log('Add user');
     }
     newUserChannel = newMember.channel;
     if (newUserChannel === null) return;
-    //let oldUserChannel = oldMember.channel;
     console.log(newMember.member.id)
     console.log(newMember.member.displayName)
     var song = users.get(newMember.member.id)
@@ -49,8 +46,8 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
             console.log("Joined vc with id " + newUserChannel);
             newUserChannel.join().then(connection => {
                 connection.play(ytdl(song)).on("finish", () => {
-                    newUserChannel.leave();
-                    console.log('played something')
+                    //newUserChannel.leave(); //need to figure this out
+                    console.log('played finish')
                 }).on("error", error => console.error(error));
             })
         } catch (err) {
@@ -74,10 +71,6 @@ client.on("message", async message => {
         users.set(userID, newSong)
         console.log(users.get(userID))
     }
-    // else if (message.content.startsWith(`${prefix}reset`)) {
-    //     songMap.set(message.guild.id, defaultSong)
-    //     console.log(songMap.get(message.guild.id))
-    // }
     else if (message.content.startsWith(`${prefix}toggle`)) {
         booleanMap.set(message.guild.id, !booleanMap.get(message.guild.id))
     }
